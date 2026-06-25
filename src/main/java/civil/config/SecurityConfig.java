775a474c24094c52c33cc227107dbe62d36ae2bd
Sachin Thapa <sachin.thapa.cs@gmail.com>
2026-06-25 18:33:56 +0545

    make change for swagger

--- src/main/java/civil/config/SecurityConfig.java
+++ src/main/java/civil/config/SecurityConfig.java
@@ -29,8 +29,14 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers(
+                                "/api/v1/auth/**",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
                         .anyRequest().authenticated()
+
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);  // ← ADD THIS
 
